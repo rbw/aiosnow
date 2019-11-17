6846c8f8719a41053637a4a4d2ad992efaeef397
Robert Wikman <rbw@vault13.org>
2019-11-17 23:55:52 +0100

    Fixes

--- fetch_parallel.py
+++ fetch_parallel.py
@@ -12,39 +12,27 @@ class Record(Schema):
     number = fields.String()
 
 
-class RequestGroup:
+class StreamGroup:
     def __init__(self, session, size, count):
         self._position = 0
         self._session = session
         self.is_depleted = False
         self.count = count
         self.size = size
-        super(RequestGroup, self).__init__()
 
     @property
     def connection(self):
         return self._session.connection
 
     @property
-    def queue(self):
-        return self._session.queue
-
-    @property
     def url(self):
         return self._session.url
 
     @property
-    def position(self):
-        return self._position
-
-    @position.setter
-    def position(self, value):
-        self._position = value
-
-    @property
     def targets(self):
         for c in range(self.count):
-            query = urlencode({"sysparm_limit": self.size, "sysparm_offset": c * (self.size + self.position)})
+            print(c * (self.size + self._position))
+            query = urlencode({"sysparm_limit": self.size, "sysparm_offset": c * (self.size + self._position)})
             yield f"{self.url}?{query}"
 
     async def execute(self):
@@ -52,17 +40,18 @@ class RequestGroup:
             *[self.connection.request("GET", url, ssl=False) for url in self.targets]
         )
 
+        print(list(self.targets))
+
         for response in responses:
             content = ujson.loads(await response.text()).get("result")
             if not content:
                 self.is_depleted = True
                 return
 
-            # yield response.status, content
-            for item in content:
+            for item in Record().load(content, many=True, unknown=EXCLUDE):
                 yield item
 
-        self.position = self.count * (self.size + self._position)
+        self._position = self.count * (self.size + self._position)
 
 
 class Session:
@@ -71,7 +60,7 @@ class Session:
 
     def __init__(self, path):
         self.url = urljoin(self.base_url, path)
-        self.group = RequestGroup(session=self, size=10, count=5)
+        self.group = StreamGroup(session=self, size=10, count=5)
 
     async def __aenter__(self):
         self.connection = aiohttp.ClientSession(
@@ -91,8 +80,8 @@ class Session:
 
 async def get_many():
     async with Session("/api/now/table/incident") as session:
-        async for result in session.pump():
-            print(result["number"])
+        async for record in session.pump():
+            pass # print(record)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
