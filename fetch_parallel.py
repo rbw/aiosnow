ddf244c5e222e72b489da27bb141afebf47a23b2
Robert Wikman <rbw@vault13.org>
2019-11-17 23:48:11 +0100

    Fixes

--- fetch_parallel.py
+++ fetch_parallel.py
@@ -1,6 +1,5 @@
 import asyncio
 
-from queue import SimpleQueue
 from urllib.parse import urljoin, urlencode
 
 import aiohttp
@@ -13,18 +12,22 @@ class Record(Schema):
     number = fields.String()
 
 
-class ExecutionQueue:
+class RequestGroup:
     def __init__(self, session, size, count):
         self._position = 0
         self._session = session
         self.is_depleted = False
         self.count = count
         self.size = size
-        # super(ExecutionQueue, self).__init__()
+        super(RequestGroup, self).__init__()
 
     @property
     def connection(self):
         return self._session.connection
+
+    @property
+    def queue(self):
+        return self._session.queue
 
     @property
     def url(self):
@@ -41,7 +44,7 @@ class ExecutionQueue:
     @property
     def targets(self):
         for c in range(self.count):
-            query = urlencode({"sysparm_limit": self.size, "sysparm_offset": c * (self.size + self._position)})
+            query = urlencode({"sysparm_limit": self.size, "sysparm_offset": c * (self.size + self.position)})
             yield f"{self.url}?{query}"
 
     async def execute(self):
@@ -55,18 +58,20 @@ class ExecutionQueue:
                 self.is_depleted = True
                 return
 
-            yield response.status, content
+            # yield response.status, content
+            for item in content:
+                yield item
 
         self.position = self.count * (self.size + self._position)
 
 
 class Session:
-    chunk_size = 1000
     base_url = ""
     connection = None
 
     def __init__(self, path):
         self.url = urljoin(self.base_url, path)
+        self.group = RequestGroup(session=self, size=10, count=5)
 
     async def __aenter__(self):
         self.connection = aiohttp.ClientSession(
@@ -78,41 +83,16 @@ class Session:
     async def __aexit__(self, exc_type, exc, tb):
         await self.connection.close()
 
-    async def get_chunked(self, limit=5, offset=0, concurrency=5):
-        """Reads chunks in parallel"""
-
-        queue = ExecutionQueue(session=self, size=50, count=5)
-        while not queue.is_depleted:
-            async for status, content in queue.execute():
-                yield status
-
-            # yield ujson.loads(await response.text()).get("result")
-
-        """def get_targets():
-            return [self._get_pages(limit=limit, offset=offset + limit) for c in range(concurrency)]
-
-        while len(self.queue):
-            for response in await asyncio.gather(*[self.connection.request("GET", url, ssl=False) for url in urls]):
-                if not result:
-                    return
-
-                yield result"""
+    async def pump(self):
+        while not self.group.is_depleted:
+            async for item in self.group.execute():
+                yield item
 
 
 async def get_many():
     async with Session("/api/now/table/incident") as session:
-        async for result in session.get_chunked():
-            print(result)
-            pass
-
-            """print(result)
-            content = Record().load(
-                data=result,
-                many=True,
-                unknown=EXCLUDE
-            )
-
-            print(content)"""
+        async for result in session.pump():
+            print(result["number"])
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
