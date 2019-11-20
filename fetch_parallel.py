9f77ab85b1777614fb6f60ada9cd4de8725b598e
Robert Wikman <rbw@vault13.org>
2019-11-20 23:09:33 +0100

    Stream parser fixes

--- fetch_parallel.py
+++ fetch_parallel.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from urllib.parse import urljoin, urlencode
+from urllib.parse import urljoin, urlencode, parse_qs
 
 import aiohttp
 import ujson
@@ -12,13 +12,13 @@ class Record(Schema):
     number = fields.String()
 
 
-class StreamGroup:
-    def __init__(self, session, size, count):
-        self._position = 0
-        self._session = session
+class FetchQueue:
+    def __init__(self, session, chunk_size, concurrency):
+        self.chunk_size = chunk_size
+        self.concurrency = concurrency
         self.is_depleted = False
-        self.count = count
-        self.size = size
+        self._group_offset = 0
+        self._session = session
 
     @property
     def connection(self):
@@ -28,43 +28,47 @@ class StreamGroup:
     def url(self):
         return self._session.url
 
-    @property
-    def targets(self):
-        for c in range(self.count):
-            print(c * (self.size + self._position))
-            query = urlencode({"sysparm_limit": self.size, "sysparm_offset": c * (self.size + self._position)})
+    def _get_next_urls(self):
+        for c in range(self.concurrency):
+            query = urlencode({
+                "sysparm_limit": self.chunk_size,
+                "sysparm_offset": self._group_offset + self.chunk_size
+            })
+
             yield f"{self.url}?{query}"
 
-    async def execute(self):
-        responses = await asyncio.gather(
-            *[self.connection.request("GET", url, ssl=False) for url in self.targets]
-        )
+    async def consume_many(self, method="GET", **kwargs):
+        targets = self._get_next_urls()
+        responses = await asyncio.gather(*[self.connection.request(method, url, **kwargs) for url in targets])
 
-        print(list(self.targets))
+        if "next" not in responses[-1].links:
+            self.is_depleted = True
+            return
+
+        group_url_next = str(responses[-1].links["next"]["url"])
+        self._group_offset = int(parse_qs(group_url_next).get("sysparm_offset")[0])
 
         for response in responses:
-            content = ujson.loads(await response.text()).get("result")
-            if not content:
-                self.is_depleted = True
-                return
-
-            for item in Record().load(content, many=True, unknown=EXCLUDE):
-                yield item
-
-        self._position = self.count * (self.size + self._position)
+            yield ujson.loads(await response.text()).get("result")
 
 
 class Session:
-    base_url = ""
+    base_url = "https://dev49212.service-now.com"
     connection = None
 
     def __init__(self, path):
         self.url = urljoin(self.base_url, path)
-        self.group = StreamGroup(session=self, size=10, count=5)
+
+    async def fetch(self, chunk_size=20, concurrency=5):
+        queue = FetchQueue(self, chunk_size, concurrency)
+        while not queue.is_depleted:
+            async for content in queue.consume_many():
+                for item in Record().load(content, many=True, unknown=EXCLUDE):
+                    yield item
 
     async def __aenter__(self):
         self.connection = aiohttp.ClientSession(
-            auth=aiohttp.helpers.BasicAuth(),
+            auth=aiohttp.helpers.BasicAuth("admin", "Bajskorv123!"),
         )
 
         return self
@@ -72,17 +76,15 @@ class Session:
     async def __aexit__(self, exc_type, exc, tb):
         await self.connection.close()
 
-    async def pump(self):
-        while not self.group.is_depleted:
-            async for item in self.group.execute():
-                yield item
-
 
 async def get_many():
     async with Session("/api/now/table/incident") as session:
-        async for record in session.pump():
-            pass # print(record)
+        async for record in session.fetch():
+            print(record)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(get_many())
+
+
+
