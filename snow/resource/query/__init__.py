ab57ad5220edddcad8104c0ba7ffc642682ec7be
Robert Wikman <rbw@vault13.org>
2020-04-21 22:21:14 +0200

    Be selective about operators imports

--- snow/resource/query/__init__.py
+++ snow/resource/query/__init__.py
@@ -1,4 +1,4 @@
 from .builder import QueryBuilder
 from .condition import Condition
-from .operators import *
+from .operators import BaseOperator, StringOperator, ReferenceField, NumericOperator, DateTimeOperator, BooleanOperator, EmailOperator, LogicalOperator
 from .utils import select
