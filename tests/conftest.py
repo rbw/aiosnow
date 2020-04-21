b67c1b16fe179944e5fc97c73075e13ea4c5018e
Robert Wikman <rbw@vault13.org>
2020-04-21 22:22:58 +0200

    Add base test code

---
+++ tests/conftest.py
@@ -0,0 +1,73 @@
+import json
+
+from aiohttp import web
+
+import pytest
+
+from snow import Application
+from snow.resource import Resource, Schema, fields
+
+
+class DefaultSchema(Schema):
+    __location__ = "/test"
+
+    test = fields.Text()
+
+
+@pytest.fixture
+def mock_client(aiohttp_client):
+    async def go(mock_server):
+        return await aiohttp_client(mock_server, server_kwargs={'skip_url_asserts': True})
+
+    yield go
+
+
+@pytest.fixture
+def mock_app(mock_client):
+    async def go(connect_to):
+        app = Application(
+            config_data=dict(
+                address="test.service-now.com",
+                basic_auth=("test", "test"),
+                use_ssl=False
+            )
+        )
+        get_session = await mock_client(connect_to)
+        app.get_session = lambda: get_session
+        return app
+
+    yield go
+
+
+@pytest.fixture
+def mock_resource(mock_server_app, mock_resource_raw):
+    async def go(method, path, *response):
+        server = mock_server_app(method, path, *response)
+        return await mock_resource_raw(connect_to=server)
+
+    yield go
+
+
+@pytest.fixture
+def mock_resource_raw(mock_app):
+    async def go(connect_to):
+        return Resource(DefaultSchema, await mock_app(connect_to))
+
+    yield go
+
+
+@pytest.fixture
+def mock_server_app():
+    def go(method, path, content, status):
+        async def handler(_):
+            return web.Response(
+                text=json.dumps(content),
+                content_type="application/json",
+                status=status
+            )
+
+        app = web.Application()
+        app.router.add_route(method, path, handler)
+        return app
+
+    yield go
---
+++ tests/request/core/test_error.py
@@ -0,0 +1,95 @@
+import pytest
+
+from snow.request import GetRequest
+from snow.exceptions import RequestError, ServerError
+
+
+async def test_error_http_ok(mock_resource):
+    """Successful HTTP requests (200) with an error body should raise a RequestError"""
+
+    response = dict(
+        error=dict(
+            message="[short msg]",
+            detail="[long msg]"
+        ),
+        status="failure"
+    ), 200
+
+    resource = await mock_resource("GET", "/", *response)
+    request = GetRequest(resource)
+
+    with pytest.raises(RequestError) as exc_info:
+        await request._send(url="/")
+
+    assert exc_info.value.message == "[short msg]: [long msg]"
+    assert exc_info.value.status == 200
+
+
+async def test_error_handled(mock_resource):
+    """HTTP error response with a body should raise RequestError"""
+
+    response = dict(
+        error=dict(
+            message="[short msg]",
+            detail="[long msg]"
+        ),
+        status="failure"
+    ), 401
+
+    resource = await mock_resource("GET", "/", *response)
+    request = GetRequest(resource)
+
+    with pytest.raises(RequestError) as exc_info:
+        await request._send(url="/")
+
+    assert exc_info.value.message == "[short msg]: [long msg]"
+    assert exc_info.value.status == 401
+
+
+async def test_error_message_only(mock_resource):
+    """HTTP error response with null detail should work"""
+
+    response = dict(
+        error=dict(
+            message="[short msg]",
+            detail=None,
+        ),
+        status="failure"
+    ), 400
+
+    resource = await mock_resource("GET", "/", *response)
+    request = GetRequest(resource)
+
+    with pytest.raises(RequestError) as exc_info:
+        await request._send(url="/")
+
+    assert exc_info.value.message == "[short msg]"
+    assert exc_info.value.status == 400
+
+
+async def test_error_fallback_500(mock_resource):
+    """HTTP error response with non-dict body should raise ServerError"""
+
+    response = "asdf", 500
+    resource = await mock_resource("GET", "/", *response)
+    request = GetRequest(resource)
+
+    with pytest.raises(ServerError) as exc_info:
+        await request._send(url="/")
+
+    assert exc_info.value.message == "Internal Server Error"
+    assert exc_info.value.status == 500
+
+
+async def test_error_fallback_503(mock_resource):
+    """HTTP error response with an empty (None) body should raise ServerError"""
+
+    response = None, 503
+    resource = await mock_resource("GET", "/", *response)
+    request = GetRequest(resource)
+
+    with pytest.raises(ServerError) as exc_info:
+        await request._send(url="/")
+
+    assert exc_info.value.message == "Service Unavailable"
+    assert exc_info.value.status == 503
