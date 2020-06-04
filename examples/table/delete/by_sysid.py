28244d5b301d110999dc1ff65dc0e2b2640cba2d
Robert Wikman <rbw@vault13.org>
2020-06-04 20:32:56 +0200

    Add examples

--- examples/table/delete/by_sysid.py
+++ examples/table/delete/by_sysid.py
@@ -3,5 +3,4 @@ from snow.schemas.table import IncidentSchema as Incident
 
 async def main(app):
     async with app.get_table(Incident) as inc:
-        # Delete by sys_id
         await inc.delete("636da7ebdb6c1010fba0560868961961")
