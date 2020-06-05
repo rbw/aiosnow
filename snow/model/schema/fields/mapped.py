f51cb2ce73b90ce06fe60b40cd25641c9064b5fd
Robert Wikman <rbw@vault13.org>
2020-06-05 14:22:33 +0200

    Add Mapping.__repr__

--- snow/model/schema/fields/mapped.py
+++ snow/model/schema/fields/mapped.py
@@ -6,28 +6,36 @@ from .integer import Integer
 from .string import String
 
 
-class StringMapping:
+class Mapping:
+    id = None
+    text = None
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} [id={self.id}, text={self.text}]>"
+
+
+class StringMapping(Mapping):
     def __init__(self, id_value: str, text_value: str):
         self.id = id_value
         self.text = text_value
 
 
-class IntegerMapping:
+class IntegerMapping(Mapping):
     def __init__(self, id_value: int, text_value: str):
         self.id = id_value
         self.text = text_value
 
 
 class MappedField(marshmallow.fields.Tuple):
+    def __init__(self, *args: Any, **kwargs: Any):
+        super(MappedField, self).__init__(self._tuple_fields, *args, **kwargs)
+
     @property
     def _tuple_fields(self) -> Tuple[Union[Integer, String], String]:
         raise NotImplementedError
 
     def _serialize(self, value: str, *args: Any, **kwargs: Any) -> Any:
         return value
-
-    def __init__(self, *args: Any, **kwargs: Any):
-        super(MappedField, self).__init__(self._tuple_fields, *args, **kwargs)
 
 
 class IntegerMap(MappedField, Integer):
