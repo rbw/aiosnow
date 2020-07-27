d7f1885e25354417c2c9fda5b3c41741f1f70fa4
Robert Wikman <rbw@vault13.org>
2020-07-27 21:48:47 +0200

    Update table.read.nested example

--- examples/table/read/nested.py
+++ examples/table/read/nested.py
@@ -20,7 +20,6 @@ async def main(app):
 
         for response in await inc.get(query):
             ag = response["assignment_group"]
-            text = "{number} is assigned to group: {ag_name} ({ag_id})".format(
+            print("{number} is assigned to group: {ag_name} ({ag_id})".format(
                 number=response["number"], ag_id=ag["sys_id"], ag_name=ag["name"]
-            )
-            print(text)
+            ))
