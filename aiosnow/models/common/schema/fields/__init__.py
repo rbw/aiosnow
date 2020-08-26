19d5d0651da62f6206ab4612b43c879f0a241cb3
Robert Wikman <rbw@vault13.org>
2020-08-26 22:43:22 +0200

    Remove email field type

--- aiosnow/models/common/schema/fields/__init__.py
+++ aiosnow/models/common/schema/fields/__init__.py
@@ -1,7 +1,6 @@
 from .base import BaseField
 from .boolean import Boolean
 from .datetime import DateTime, Datetime
-from .email import Email
 from .integer import Integer, Numeric
 from .mapped import IntegerMap, StringMap
 from .string import String, Text
