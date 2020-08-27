1e3022af61b0dc52dbe2c243cec9619716de5402
Robert Wikman <rbw@vault13.org>
2020-08-27 21:08:32 +0200

    Add more field tests

--- tests/fields/test_fields_datetime.py
+++ tests/fields/test_fields_datetime.py
@@ -1,41 +1,90 @@
 import pytest
 
-from aiosnow.models.common.schema import BaseSchema, fields
 from aiosnow.exceptions import DeserializationError
+from aiosnow.models.common.schema import BaseSchema, fields
+from aiosnow.query.utils import select
 
 
-async def test_fields_datetime_type(mock_model):
+def test_fields_datetime_type():
     class TestSchema(BaseSchema):
-        f_dt1 = fields.DateTime()
+        test_dt = fields.DateTime()
 
     schema = TestSchema()
-    assert isinstance(schema.f_dt1, fields.DateTime)
+    assert isinstance(schema.test_dt, fields.DateTime)
 
 
-async def test_fields_datetime_deserialize_plain_valid(mock_model):
+def test_fields_datetime_deserialize_plain_valid():
     class TestSchema(BaseSchema):
-        f_dt1 = fields.DateTime()
-        f_dt2 = fields.DateTime()
+        test_dt1 = fields.DateTime()
+        test_dt2 = fields.DateTime()
 
     schema = TestSchema()
-    data = schema.load({
-        "f_dt1": "2020-08-26 21:01:07",
-        "f_dt2": "2020-08-26T21:01:07+00:00"
-    })
+    data = schema.load(
+        {"test_dt1": "2020-08-26 21:01:07", "test_dt2": "2020-08-26T21:01:07+00:00"}
+    )
 
-    assert data["f_dt1"].year == data["f_dt2"].year == 2020
-    assert data["f_dt1"].month == data["f_dt2"].month == 8
-    assert data["f_dt1"].day == data["f_dt2"].day == 26
-    assert data["f_dt1"].hour == data["f_dt2"].hour == 21
-    assert data["f_dt1"].minute == data["f_dt2"].minute == 1
-    assert data["f_dt1"].second == data["f_dt2"].second == 7
+    assert data["test_dt1"].year == data["test_dt2"].year == 2020
+    assert data["test_dt1"].month == data["test_dt2"].month == 8
+    assert data["test_dt1"].day == data["test_dt2"].day == 26
+    assert data["test_dt1"].hour == data["test_dt2"].hour == 21
+    assert data["test_dt1"].minute == data["test_dt2"].minute == 1
+    assert data["test_dt1"].second == data["test_dt2"].second == 7
 
 
-async def test_fields_datetime_deserialize_plain_invalid(mock_model):
+def test_fields_datetime_deserialize_plain_invalid():
     class TestSchema(BaseSchema):
-        f_dt1 = fields.DateTime()
+        test_dt = fields.DateTime()
 
     schema = TestSchema()
 
     with pytest.raises(DeserializationError):
-        schema.load({"f_dt1": "1234-12-56 50:20:30"})
+        schema.load({"test_dt": "1234-12-56 50:20:30"})
+
+
+def test_fields_datetime_after():
+    class TestSchema(BaseSchema):
+        test_dt = fields.DateTime()
+
+    assert select(TestSchema.test_dt.after("2019-12-24 02:03:04")).sysparms == "test_dt>2019-12-24 02:03:04"
+
+
+def test_fields_datetime_as_of():
+    class TestSchema(BaseSchema):
+        test_dt = fields.DateTime()
+
+    assert select(TestSchema.test_dt.as_of("2019-12-24 02:03:04")).sysparms == "test_dt>=2019-12-24 02:03:04"
+
+
+def test_fields_datetime_before():
+    class TestSchema(BaseSchema):
+        test_dt = fields.DateTime()
+
+    assert select(TestSchema.test_dt.before("2019-12-24 02:03:04")).sysparms == "test_dt<2019-12-24 02:03:04"
+
+
+def test_fields_datetime_between():
+    class TestSchema(BaseSchema):
+        test_dt = fields.DateTime()
+
+    assert select(TestSchema.test_dt.between("2018-12-24 02:03:04", "2019-12-24 02:03:04")).sysparms == "test_dtBETWEEN2018-12-24 02:03:04@2019-12-24 02:03:04"
+
+
+def test_fields_datetime_not_on():
+    class TestSchema(BaseSchema):
+        test_dt = fields.DateTime()
+
+    assert select(TestSchema.test_dt.not_on("2019-12-24 02:03:04")).sysparms == "test_dtNOTON2019-12-24 02:03:04"
+
+
+def test_fields_datetime_on():
+    class TestSchema(BaseSchema):
+        test_dt = fields.DateTime()
+
+    assert select(TestSchema.test_dt.on("2019-12-24 02:03:04")).sysparms == "test_dtON2019-12-24 02:03:04"
+
+
+def test_fields_datetime_until():
+    class TestSchema(BaseSchema):
+        test_dt = fields.DateTime()
+
+    assert select(TestSchema.test_dt.until("2019-12-24 02:03:04")).sysparms == "test_dt<=2019-12-24 02:03:04"
--- tests/fields/test_fields_integer.py
+++ tests/fields/test_fields_integer.py
@@ -1,53 +1,97 @@
 import pytest
 
-from aiosnow.models.common.schema import BaseSchema, fields
 from aiosnow.exceptions import DeserializationError
+from aiosnow.models.common.schema import BaseSchema, fields
+from aiosnow.query.utils import select
 
 
-async def test_fields_integer_types(mock_model):
+def test_fields_integer_types():
     class TestSchema(BaseSchema):
-        f_int1 = fields.Integer()
-        f_int2 = fields.Integer()
-        f_intmap1 = fields.IntegerMap()
+        test_int1 = fields.Integer()
+        test_int2 = fields.Integer()
+        test_intmap = fields.IntegerMap()
 
     schema = TestSchema()
-    assert isinstance(schema.f_int1, fields.Integer)
-    assert isinstance(schema.f_int2, fields.Integer)
-    assert isinstance(schema.f_intmap1, fields.IntegerMap)
+    assert isinstance(schema.test_int1, fields.Integer)
+    assert isinstance(schema.test_int2, fields.Integer)
+    assert isinstance(schema.test_intmap, fields.IntegerMap)
 
 
-async def test_fields_integer_deserialize_plain_valid(mock_model):
+def test_fields_integer_deserialize_plain_valid():
     class TestSchema(BaseSchema):
-        f_int1 = fields.Integer()
+        test_int = fields.Integer()
 
     schema = TestSchema()
-    data = schema.load({
-        "f_int1": 123
-    })
+    data = schema.load({"test_int": 123})
 
-    assert data["f_int1"] == 123
+    assert data["test_int"] == 123
 
 
-async def test_fields_integer_deserialize_plain_invalid(mock_model):
+def test_fields_integer_deserialize_plain_invalid():
     class TestSchema(BaseSchema):
-        f_int1 = fields.Integer()
+        test_int = fields.Integer()
 
     schema = TestSchema()
 
     with pytest.raises(DeserializationError):
-        schema.load({"f_int1": "asdf"})
+        schema.load({"test_int": "asdf"})
 
 
-async def test_fields_integer_deserialize_mapping(mock_model):
+def test_fields_integer_deserialize_mapping():
     class TestSchema(BaseSchema):
         test_int = fields.IntegerMap()
 
     schema = TestSchema()
-    data = schema.load(
-        {
-            "test_int": (3, "3 - High")
-        }
-    )
+    data = schema.load({"test_int": (3, "3 - High")})
 
     assert data["test_int"].key == 3
     assert data["test_int"].value == "3 - High"
+
+
+def test_fields_integer_query_between():
+    class TestSchema(BaseSchema):
+        test_int = fields.Integer()
+
+    assert select(TestSchema.test_int.between(1, 3)).sysparms == "test_intBETWEEN1@3"
+
+
+def test_fields_integer_query_equals():
+    class TestSchema(BaseSchema):
+        test_int = fields.Integer()
+
+    assert select(TestSchema.test_int.equals(0)).sysparms == "test_int=0"
+
+
+def test_fields_integer_greater_or_equals():
+    class TestSchema(BaseSchema):
+        test_int = fields.Integer()
+
+    assert select(TestSchema.test_int.greater_or_equals(0)).sysparms == "test_int>=0"
+
+
+def test_fields_integer_greater_than():
+    class TestSchema(BaseSchema):
+        test_int = fields.Integer()
+
+    assert select(TestSchema.test_int.greater_than(2)).sysparms == "test_int>2"
+
+
+def test_fields_integer_less_or_equals():
+    class TestSchema(BaseSchema):
+        test_int = fields.Integer()
+
+    assert select(TestSchema.test_int.less_or_equals(2)).sysparms == "test_int<=2"
+
+
+def test_fields_integer_less_than():
+    class TestSchema(BaseSchema):
+        test_int = fields.Integer()
+
+    assert select(TestSchema.test_int.less_than(2)).sysparms == "test_int<2"
+
+
+def test_fields_integer_not_equals():
+    class TestSchema(BaseSchema):
+        test_int = fields.Integer()
+
+    assert select(TestSchema.test_int.not_equals(2)).sysparms == "test_int!=2"
