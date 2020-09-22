5e0581808015dea54b402ba57c632d2d60bfadfe
Robert Wikman <rbw@vault13.org>
2020-09-22 23:06:27 +0200

    Separate querying methods from fields
    Adds new type <Field>Queryable: a mixin that
    can be used with BaseField to make fields queryable

--- aiosnow/models/common/schema/fields/boolean.py
+++ aiosnow/models/common/schema/fields/boolean.py
@@ -1,25 +1,9 @@
 import marshmallow
 
-from aiosnow.query import BooleanOperator, Condition
+from aiosnow.query import BooleanQueryable
 
 from .base import BaseField
 
 
-class Boolean(marshmallow.fields.Boolean, BaseField):
-    def is_true(self) -> Condition:
-        """
-        Example: active.is_true()
-
-        All records in which the Active field is True.
-        """
-
-        return self._condition(BooleanOperator.EQUALS, "true")
-
-    def is_falsy(self) -> Condition:
-        """
-        Example: active.is_falsy()
-
-        All records in which the Active field is False, empty, or NULL.
-        """
-
-        return self._condition(BooleanOperator.NOT_EQUALS, "true")
+class Boolean(marshmallow.fields.Boolean, BaseField, BooleanQueryable):
+    pass
