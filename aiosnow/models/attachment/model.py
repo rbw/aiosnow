28dcbfd714fdf533489b9f987e584bbd511e6b8f
Robert Wikman <rbw@vault13.org>
2020-10-14 23:04:13 +0200

    Add upload method

--- aiosnow/models/attachment/model.py
+++ aiosnow/models/attachment/model.py
@@ -1,14 +1,16 @@
 import asyncio
-
-from typing import Union
 from concurrent.futures import ThreadPoolExecutor
+from mimetypes import guess_type
+from typing import Any, Union
 
+from aiosnow.client import Client
 from aiosnow.models.common import fields
 from aiosnow.models.table import TableModel
-from aiosnow.request import methods
 from aiosnow.query import Condition, Selector
+from aiosnow.request import methods
+from aiosnow.request.response import ClientResponse
 
-from .file import FileWriter
+from .file import FileHandler, FileReader, FileWriter
 
 
 class AttachmentModel(TableModel):
@@ -28,7 +30,7 @@ class AttachmentModel(TableModel):
     download_link = fields.String()
     content_type = fields.String()
     sys_created_on = fields.DateTime()
-    size_compressed = fields.Integer()
+    size_compressed = fields.String()
     compressed = fields.Boolean()
     state = fields.String()
     table_sys_id = fields.String()
@@ -36,27 +38,46 @@ class AttachmentModel(TableModel):
     hash = fields.String()
     sys_created_by = fields.String()
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, client: Client, table_name: str, return_only: list = None):
         self.io_pool_exc = ThreadPoolExecutor(max_workers=10)
         self.loop = asyncio.get_running_loop()
-        super(AttachmentModel, self).__init__(*args, table_name="", **kwargs)
+        super(AttachmentModel, self).__init__(client, table_name, return_only)
 
     @property
     def _api_url(self) -> str:
         return self._client.base_url + "/api/now/attachment"
 
-    async def download(self, selection: Union[Selector, Condition, str], dst_dir="."):
-        attachment = await self.get_one(selection)
-        response = await self._session.request(
+    async def download(
+        self, selection: Union[Selector, Condition, str], dst_dir: str = "."
+    ) -> FileHandler:
+        meta = await self.get_one(selection)
+        data = await self._session.request(
             methods.GET,
-            url=attachment["download_link"],
-            headers={"Content-type": attachment["content_type"], "Accept": "*/*"},
+            url=meta["download_link"],
+            headers={"Content-type": meta["content_type"]},
         )
-        file = FileWriter(attachment["file_name"])
-        with file as f:
-            await self.loop.run_in_executor(self.io_pool_exc, f.write, await response.read())
+        with FileWriter(meta["file_name"], dst_dir) as f:
+            await self.loop.run_in_executor(
+                self.io_pool_exc, f.write, await data.read()
+            )
 
-        return file
+        return f
 
-    async def upload(self):
-        pass
+    async def upload(
+        self, table_name: str, record_sys_id: str, file_name: str, **kwargs: Any
+    ) -> ClientResponse:
+        with FileReader(file_name, **kwargs) as f:
+            content = await self.loop.run_in_executor(self.io_pool_exc, f.read)
+
+        content_type, _ = guess_type(file_name)
+        response = await self._session.request(
+            methods.POST,
+            url=f"{self._api_url}/file",
+            params=dict(
+                table_name=table_name, table_sys_id=record_sys_id, file_name=file_name
+            ),
+            headers={"Content-type": content_type},
+            data=content,
+        )
+
+        return response
