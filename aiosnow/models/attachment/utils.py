7911429612e1af101963fd05947e9d03f2994cf0
Robert Wikman <rbw@vault13.org>
2020-10-14 23:05:07 +0200

    Improve attachment.utils:convert_size readability

--- aiosnow/models/attachment/utils.py
+++ aiosnow/models/attachment/utils.py
@@ -1,12 +1,13 @@
 import math
+from typing import Tuple
 
 
-def convert_size(size_bytes):
+def convert_size(size_bytes: int) -> Tuple[float, str]:
     if size_bytes == 0:
         return 0, "B"
 
-    u = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return s, u[i]
+    units = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    log = int(math.floor(math.log(size_bytes, 1024)))
+    power = math.pow(1024, log)
+    size = round(size_bytes / power, 2)
+    return size, units[log]
