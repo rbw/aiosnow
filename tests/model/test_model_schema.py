b6df460f1ab1e6b89ae07a03c089cdb432f18eb0
Robert Wikman <rbw@vault13.org>
2020-10-28 00:07:53 +0100

    Add ModelSchema tests

---
+++ tests/model/test_model_schema.py
@@ -0,0 +1,45 @@
+import pytest
+
+from aiosnow.models import fields, ModelSchema
+from aiosnow.query.fields import StringQueryable, IntegerQueryable
+from aiosnow.exceptions import SchemaError
+
+
+def test_model_schema_field_registration():
+    class TestSchema(ModelSchema):
+        test1 = fields.String()
+        test2 = fields.Integer()
+
+    assert isinstance(TestSchema.test1, StringQueryable)
+    assert isinstance(TestSchema.test2, IntegerQueryable)
+    assert isinstance(TestSchema.fields["test1"], fields.String)
+    assert isinstance(TestSchema.fields["test2"], fields.Integer)
+
+
+def test_model_schema_primary_key():
+    with pytest.raises(SchemaError):
+        type("TestSchema", (ModelSchema, ), dict(
+            test1=fields.String(is_primary=True),
+            test2=fields.Integer(is_primary=True)
+        ))
+
+
+def test_model_schema_nested():
+    pass
+
+
+def test_model_schema_dumps():
+    pass
+
+
+def test_model_schema_loads():
+    pass
+
+
+def test_model_schema_load_response_content():
+    pass
+
+
+def test_model_schema_dump_request_payload():
+    pass
+
