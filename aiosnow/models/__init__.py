133c1e42addc6d783d4f08c3f0d6c9c1f19eaf29
Robert Wikman <rbw@vault13.org>
2020-11-16 22:37:43 +0100

    Export base classes

--- aiosnow/models/__init__.py
+++ aiosnow/models/__init__.py
@@ -1,3 +1,5 @@
 from ._schema import BaseField, ModelSchema, ModelSchemaMeta, Pluck, fields
+from ._base import BaseTableModel, BaseModelMeta, BaseModel
 from .attachment import AttachmentModel
 from .table import TableModel
+
